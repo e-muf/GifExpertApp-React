5d9d3a83bfbb88b085f0836ac235dc4e711409de
Emanuel Flores <mufymes@gmail.com>
2020-07-30 23:29:46 -0500

    Docs agregado

---
+++ docs/precache-manifest.edfd543143fe2337572638ba5676d044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003c053e04168848752fb458aae2ae88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d490e530c692083d78",
+    "url": "/static/css/main.6d8e49b0.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d490e530c692083d78",
+    "url": "/static/js/main.ee302438.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
